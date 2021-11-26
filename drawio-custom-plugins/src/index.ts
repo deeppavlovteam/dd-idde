345d6a7b942c6f8961c3d287d4dac3b1d6033039
mablin7 <magyarb0201@gmail.com>
2021-11-26 15:32:59 +0300

    a fresh start

--- drawio-custom-plugins/src/index.ts
+++
@@ -1,9 +0,0 @@
-import "./linkSelectedNodeWithData";
-import "./liveshare";
-import "./focus";
-import "./menu-entries";
-import "./donationDialog";
-
-Draw.loadPlugin((ui) => {
-	(window as any).hediet_DbgUi = ui;
-});
